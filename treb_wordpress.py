4e8cd08ab30d72cf860ed437e370abf815ecd836
root <root@stardothosting.com>
2013-07-19 12:50:58 -0400

    Changes to the code :
    - Added logic to process available and unavailable listings
    - Added function to unlist a wordpress post based on the MLS number contained in the post content

--- treb_wordpress.py
+++ treb_wordpress.py
@@ -85,7 +85,7 @@ def ftpget( hostname, localpath, remotepath, filename ) :
 #Searches wordpress posts based on title
 def find_id(title):
         offset = 0
-        increment = 10
+        increment = 20
         while True:
                 filter = { 'offset' : offset }
                 p = wp.call(GetPosts(filter))
@@ -97,6 +97,22 @@ def find_id(title):
                 offset = offset + increment
         return(False)
 
+#Searches wordpress post content for MLS number (or anything else)
+def unlist_mls(mlsnum):
+	offset = 0
+	increment = 20
+	while True:
+        	filter = { 'offset' : offset }
+        	p = client.call(GetPosts(filter))
+        	if len(p) == 0:
+                	break # no more posts returned
+        	for post in p:
+                	if post.content.find(mlsnum) != -1:
+                        	post.post_status = 'unpublish'
+                        	client.call(posts.EditPost(post.id, post))
+				return(post.id)
+        	offset = offset + increment
+	return(False)
 
 #Take text and replace words that match an array
 def replace_words(text, word_dic):
@@ -118,6 +134,7 @@ password = ConfigSectionMap("treb")['trebpass']
 agent_id = ConfigSectionMap("treb")['agent_id']
 #int_date = Config.getint('treb', 'num_days')
 int_date = int(sys.argv[1])
+avail_opt = int(sys.argv[0])
 rootdir = ConfigSectionMap("treb")['root_dir']
 userperm = Config.getint('wordpress', 'user_perm')
 groupperm = Config.getint('wordpress', 'group_perm')
@@ -138,135 +155,162 @@ else:
 	print rootdir
 
 # build the url query string
-url = "http://3pv.torontomls.net/data3pv/DownLoad3PVAction.asp?user_code=" + user + "&password=" + password + "&sel_fields=*&dlDay=" + the_day + "&dlMonth=" + the_mon + "&dlYear=" + the_yr + "&order_by=&au_both=avail&dl_type=file&incl_names=yes&use_table=MLS&send_done=no&submit1=Submit&query_str=lud%3E%3D%27" + the_yr + the_mon + the_day + "%27"
+url = "http://3pv.torontomls.net/data3pv/DownLoad3PVAction.asp?user_code=" + user + "&password=" + password + "&sel_fields=*&dlDay=" + the_day + "&dlMonth=" + the_mon + "&dlYear=" + the_yr + "&order_by=&au_both=" + avail_opt + "&dl_type=file&incl_names=yes&use_table=MLS&send_done=no&submit1=Submit&query_str=lud%3E%3D%27" + the_yr + the_mon + the_day + "%27"
 
 # retrieve URL and  write results to filename
 filename = "/tmp/out_py.txt"
 urllib.urlretrieve(url,filename)
 
 
-# read the csv file
-f = open('/tmp/out_py.txt', 'r') #open file
-try:
-    r = csv.reader(f) #init csv reader
-    r.next()
-    for row in r:
-        description = row[2]
-        streetname = row[273]
-        streetnumber = row[275]
-	streetsuffix = row[276]
-	address = row[3]
-        postalcode = row[330]
-        bathrooms = row[10]
-        bedrooms = row[15]
-        bedplus = row[16]
-        houseclass = row[29]
-        extras = row[69]
-        listagent = row[111]
-        listprice = row[120]
-        mlsnumber = row[130]
-        squarefoot = row[269]
-        virtualtour = row[292]
-        pictures = row[174]
-        inputdate = row[174]
-        lastupdate = row[123]
-        solddate = row[24]
-        agentid = row[333]
+# Available or Unavailable Listing Logic
+if avail_opt == "avail":
 
-	#Sanitize Variables
-	listpricefix = locale.currency(int(listprice), grouping=True )
-#	description = description.replace(":", "\:").replace("/", "\/").replace("&", "\&")
-#	virtualtour = virtualtour.replace(":", "\:").replace("/", "\/").replace("&", "\&")
+	# read the csv file
+	f = open('/tmp/out_py.txt', 'r') #open file
+	try:
+    		r = csv.reader(f) #init csv reader
+    		r.next()
+    		for row in r:
+        		description = row[2]
+        		streetname = row[273]
+        		streetnumber = row[275]
+			streetsuffix = row[276]
+			address = row[3]
+        		postalcode = row[330]
+        		bathrooms = row[10]
+        		bedrooms = row[15]
+        		bedplus = row[16]
+        		houseclass = row[29]
+        		extras = row[69]
+        		listagent = row[111]
+        		listprice = row[120]
+        		mlsnumber = row[130]
+        		squarefoot = row[269]
+        		virtualtour = row[292]
+        		pictures = row[174]
+        		inputdate = row[174]
+        		lastupdate = row[123]
+        		solddate = row[24]
+        		agentid = row[333]
 
-	# Set category and verify if other agent listing is over $600,0000
-	if agentid == agent_id:
-                listingcategory = "Listings"
-        else:
-		if int(listprice) < 600000:
-                        print "Listing is below $600,000 - Not adding"
-                        continue
-                else:
-                        listingcategory = "OtherListings"
+			#Sanitize Variables
+			listpricefix = locale.currency(int(listprice), grouping=True )
+			#description = description.replace(":", "\:").replace("/", "\/").replace("&", "\&")
+			#virtualtour = virtualtour.replace(":", "\:").replace("/", "\/").replace("&", "\&")
 
-        # Get the latitude + longitude variables
-	results = Geocoder.geocode(address + " Toronto, Ontario, Canada")
-	lat, lng = results[0].coordinates	
-	lat = str(lat)
-	lng = str(lng)
 
-        # Set variable for virtual tour
-	if virtualtour == "":
-		virtualtour = "N/A"
-	else:
-		virtualtour = "<a href=\"" + virtualtour + "\" target=\"_new\"><b>Click here for virtual tour</b></a>"
+		# Set category and verify if other agent listing is over $600,0000
+		if agentid == agent_id:
+                	listingcategory = "Listings"
+        	else:
+			if int(listprice) < 600000:
+                        	print "Listing is below $600,000 - Not adding"
+                        	continue
+                	else:
+                        	listingcategory = "OtherListings"
 
-	# Start prepping the uploads folder for the MLS listing images
-	if pictures == "Y":
-		mlsimage = mlsnumber[-3:]
-		print "MLS Image : " + mlsimage
+        	# Get the latitude + longitude variables
+		results = Geocoder.geocode(address + " Toronto, Ontario, Canada")
+		lat, lng = results[0].coordinates	
+		lat = str(lat)
+		lng = str(lng)
 
-		# Create upload folder if it doesnt exist
-		if not os.path.exists(rootdir + '/wp-content/uploads/treb/' + mlsnumber):
-    			os.makedirs(rootdir + '/wp-content/uploads/treb/' + mlsnumber)
+        	# Set variable for virtual tour
+		if virtualtour == "":
+			virtualtour = "N/A"
+		else:
+			virtualtour = "<a href=\"" + virtualtour + "\" target=\"_new\"><b>Click here for virtual tour</b></a>"
 
-		# GET The image files via FTP
-		ftpget( "3pv.torontomls.net", rootdir + "/wp-content/uploads/treb/" + mlsnumber, "mlsmultiphotos/1/" + mlsimage, mlsnumber + ".jpg")
-		ftpget( "3pv.torontomls.net", rootdir + "/wp-content/uploads/treb/" + mlsnumber, "mlsmultiphotos/2/" + mlsimage, mlsnumber + "_2.jpg")
-		ftpget( "3pv.torontomls.net", rootdir + "/wp-content/uploads/treb/" + mlsnumber, "mlsmultiphotos/3/" + mlsimage, mlsnumber + "_3.jpg")
-		ftpget( "3pv.torontomls.net", rootdir + "/wp-content/uploads/treb/" + mlsnumber, "mlsmultiphotos/4/" + mlsimage, mlsnumber + "_4.jpg")
-		ftpget( "3pv.torontomls.net", rootdir + "/wp-content/uploads/treb/" + mlsnumber, "mlsmultiphotos/5/" + mlsimage, mlsnumber + "_5.jpg")
-		ftpget( "3pv.torontomls.net", rootdir + "/wp-content/uploads/treb/" + mlsnumber, "mlsmultiphotos/6/" + mlsimage, mlsnumber + "_6.jpg")
-		ftpget( "3pv.torontomls.net", rootdir + "/wp-content/uploads/treb/" + mlsnumber, "mlsmultiphotos/7/" + mlsimage, mlsnumber + "_7.jpg")
-		ftpget( "3pv.torontomls.net", rootdir + "/wp-content/uploads/treb/" + mlsnumber, "mlsmultiphotos/8/" + mlsimage, mlsnumber + "_8.jpg")
-		ftpget( "3pv.torontomls.net", rootdir + "/wp-content/uploads/treb/" + mlsnumber, "mlsmultiphotos/9/" + mlsimage, mlsnumber + "_9.jpg")
+		# Start prepping the uploads folder for the MLS listing images
+		if pictures == "Y":
+			mlsimage = mlsnumber[-3:]
+			print "MLS Image : " + mlsimage
+
+			# Create upload folder if it doesnt exist
+			if not os.path.exists(rootdir + '/wp-content/uploads/treb/' + mlsnumber):
+    				os.makedirs(rootdir + '/wp-content/uploads/treb/' + mlsnumber)
+
+			# GET The image files via FTP
+			ftpget( "3pv.torontomls.net", rootdir + "/wp-content/uploads/treb/" + mlsnumber, "mlsmultiphotos/1/" + mlsimage, mlsnumber + ".jpg")
+			ftpget( "3pv.torontomls.net", rootdir + "/wp-content/uploads/treb/" + mlsnumber, "mlsmultiphotos/2/" + mlsimage, mlsnumber + "_2.jpg")
+			ftpget( "3pv.torontomls.net", rootdir + "/wp-content/uploads/treb/" + mlsnumber, "mlsmultiphotos/3/" + mlsimage, mlsnumber + "_3.jpg")
+			ftpget( "3pv.torontomls.net", rootdir + "/wp-content/uploads/treb/" + mlsnumber, "mlsmultiphotos/4/" + mlsimage, mlsnumber + "_4.jpg")
+			ftpget( "3pv.torontomls.net", rootdir + "/wp-content/uploads/treb/" + mlsnumber, "mlsmultiphotos/5/" + mlsimage, mlsnumber + "_5.jpg")
+			ftpget( "3pv.torontomls.net", rootdir + "/wp-content/uploads/treb/" + mlsnumber, "mlsmultiphotos/6/" + mlsimage, mlsnumber + "_6.jpg")
+			ftpget( "3pv.torontomls.net", rootdir + "/wp-content/uploads/treb/" + mlsnumber, "mlsmultiphotos/7/" + mlsimage, mlsnumber + "_7.jpg")
+			ftpget( "3pv.torontomls.net", rootdir + "/wp-content/uploads/treb/" + mlsnumber, "mlsmultiphotos/8/" + mlsimage, mlsnumber + "_8.jpg")
+			ftpget( "3pv.torontomls.net", rootdir + "/wp-content/uploads/treb/" + mlsnumber, "mlsmultiphotos/9/" + mlsimage, mlsnumber + "_9.jpg")
 		
-		# Adjust permissions , change 33 to whatever GID/UID you need
-		os.chown(rootdir + "/wp-content/uploads/treb/" + mlsnumber, userperm, groupperm)
-		for root, dirs, files in os.walk(rootdir + "/wp-content/uploads/treb/" + mlsnumber):
-			for filename in files:
-				os.chown(os.path.join(root, filename), userperm, groupperm)
-        else:
-                print "No photos ..."
+			# Adjust permissions , change 33 to whatever GID/UID you need
+			os.chown(rootdir + "/wp-content/uploads/treb/" + mlsnumber, userperm, groupperm)
+			for root, dirs, files in os.walk(rootdir + "/wp-content/uploads/treb/" + mlsnumber):
+				for filename in files:
+					os.chown(os.path.join(root, filename), userperm, groupperm)
+        	else:
+                	print "No photos ..."
 
-        # Generate post content from the template file
-	template_read = open("./listing_template.txt", "r")
-	template_text = template_read.read()
-	template_read.close()
+        	# Generate post content from the template file
+		template_read = open("./listing_template.txt", "r")
+		template_text = template_read.read()
+		template_read.close()
 
-	#Replacements from the template
-	reps = {'%STREETNUMBER%':streetnumber, '%STREETNAME%':streetname + ' ' + streetsuffix, '%POSTALCODE%':postalcode, '%LISTPRICE%':listpricefix, '%MLSNUMBER%':mlsnumber, '%BATHROOMS%':bathrooms, '%BEDROOMS%':bedrooms, '%SQFOOTAGE%':squarefoot, '%DESCRIPTION%':description, '%VIRTUALTOUR%':virtualtour}
+		#Replacements from the template
+		reps = {'%STREETNUMBER%':streetnumber, '%STREETNAME%':streetname + ' ' + streetsuffix, '%POSTALCODE%':postalcode, '%LISTPRICE%':listpricefix, '%MLSNUMBER%':mlsnumber, '%BATHROOMS%':bathrooms, '%BEDROOMS%':bedrooms, '%SQFOOTAGE%':squarefoot, '%DESCRIPTION%':description, '%VIRTUALTOUR%':virtualtour}
 
-	# Prepare the post
-	wp = Client(wp_url, wp_username, wp_password)
-	post = WordPressPost()
-	post.title = address
-	post.content = replace_words(template_text, reps)
-	post.terms_names = {
-        'post_tag': [mlsnumber],
-        'category': ['My Child Category'],
-	}
+		# Prepare the post
+		wp = Client(wp_url, wp_username, wp_password)
+		post = WordPressPost()
+		post.title = address
+		post.content = replace_words(template_text, reps)
+		post.terms_names = {
+        	'post_tag': [mlsnumber],
+        	'category': ['My Child Category'],
+		}
 	
-	# Check if post exists already
-	print "Post title : " + post.title
-	post_id = find_id(post.title)
-	if post_id:
-		# check if sold date variable is set and update existing post to reflect the property as sold
-		if solddate == "" :
-			print "Sorry, a post ID exists already with that title: ", post_id
-		else :
-			post.title = "[SOLD!] " + post.title
-			wp.call(posts.EditPost(post.id, post))
+		# Check if post exists already
+		print "Post title : " + post.title
+		post_id = find_id(post.title)
+		if post_id:
+			# check if sold date variable is set and update existing post to reflect the property as sold
+			if solddate == "" :
+				print "Sorry, a post ID exists already with that title: ", post_id
+			else :
+				post.title = "[SOLD!] " + post.title
+				wp.call(posts.EditPost(post.id, post))
 			
-	else:
-		#Output text to a post file to be eventually posted to wordpress	
-		template_out = open("./metadata/" + mlsnumber + "_post.txt", "w")
-		template_out.write(post.content)
-		template_out.close()
-		post.id = wp.call(NewPost(post))
-		post.post_status = 'publish'
-		wp.call(posts.EditPost(post.id, post))
+		else:
+			#Output text to a post file to be eventually posted to wordpress	
+			template_out = open("./metadata/" + mlsnumber + "_post.txt", "w")
+			template_out.write(post.content)
+			template_out.close()
+			post.id = wp.call(NewPost(post))
+			post.post_status = 'publish'
+			wp.call(posts.EditPost(post.id, post))
+	finally:
+		f.close() #cleanup
+	
+	# Unavailable option
+elif avail_opt == "unavail" :
 
-finally:
-    f.close() #cleanup
+        # read the csv file
+        f = open('/tmp/out_py.txt', 'r') #open file
+        try:
+                r = csv.reader(f) #init csv reader
+                r.next()
+                for row in r:
+                        mlsnumber = row[1]
+		
+	# Prepare post title for search
+	wp = Client(wp_url, wp_username, wp_password)
+	unavail_id = unlist_mls(mlsnumber)
+	if unavail_id : 
+		print "The following post ID has been unpublished : " + unavail_id
+	else
+		print "No matches for any posts with the MLS " + mlsnumber
 
+	finally:
+		f.close() #cleanup
 
+else :
+	print "Invalid command options given"
+	sys.exit(0)
